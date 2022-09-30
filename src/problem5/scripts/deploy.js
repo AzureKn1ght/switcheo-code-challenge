050a657489889f55495d22213d18727a3b34a4bc
AzureKn1ght <philsonnah@msn.com>
2022-09-30 18:22:33 +0800

    Added hardhat boilerplate

---
+++ src/problem5/scripts/deploy.js
@@ -0,0 +1,30 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts, so myContract here is a factory for instances of our contract.
+  */
+  const myContract = await ethers.getContractFactory("MyContract");
+
+  // here we deploy the contract
+  const deployedContract = await myContract.deploy();
+  // you can also pass in parameters for contructor
+
+  // Wait for it to finish deploying
+  await deployedContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("Contract Address:", deployedContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+//VERIFY SOURCE CODE ON ETHERSCAN
+//npx hardhat verify --network goerli <address>
